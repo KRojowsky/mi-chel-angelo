7d12edecad21efbf46ec90301f6028d8f4973efc
KRojowsky <rojowsky@poczta.fm>
2024-07-17 14:26:46 +0200

    creating mi-bot chatbot json

--- creator/static/js/chat.js
+++ creator/static/js/chat.js
@@ -27,6 +27,7 @@ function appendUserMessage(message) {
     messageDiv.innerHTML = `
         <div class="message-text">
             <p>${message}</p>
+            <div class="message-time">${getCurrentTime()}</div>
         </div>`;
     chatMessages.appendChild(messageDiv);
     chatMessages.scrollTop = chatMessages.scrollHeight;
@@ -38,12 +39,46 @@ function addUserMessage(messageText) {
 }
 
 function checkUserMessage(messageText) {
-    const lowerCaseMessage = messageText.toLowerCase();
-    if (lowerCaseMessage.includes('sprawdź status zamówienia') || lowerCaseMessage.includes('status zamówienia')) {
-        setTimeout(function() {
-            sendMessageFromBot('Podaj numer zamówienia:');
-        }, 1000);
-    }
+    const normalizedMessage = normalizeText(messageText);
+
+    fetch('/static/json/responses.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Wystąpiły problemy z siecią....');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const { questions } = data;
+            let matchedQuestion = null;
+
+            questions.forEach(item => {
+                item.question.forEach(q => {
+                    const normalizedQuestion = normalizeText(q);
+                    if (normalizedMessage.includes(normalizedQuestion)) {
+                        matchedQuestion = item;
+                    }
+                });
+            });
+
+            if (matchedQuestion) {
+                const { answers } = matchedQuestion;
+                const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+                sendMessageFromBot(randomAnswer);
+            } else {
+                sendMessageFromBot("Przykro mi, ale niestety nie jestem w stanie udzielić Ci pomocy. Spróbuj skontaktować się z nami mailowo: <br><b>sklep@mi-store.pl</b><br> lub telefoniczne: <br><b>575 228 666</b>.&#128151;");
+            }
+        })
+        .catch(error => console.error('Błąd przetwarzania treści:', error));
+}
+
+function normalizeText(text) {
+    return text.toLowerCase()
+               .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+               .replace(/ą/g, 'a').replace(/ć/g, 'c').replace(/ę/g, 'e')
+               .replace(/ł/g, 'l').replace(/ń/g, 'n').replace(/ó/g, 'o')
+               .replace(/ś/g, 's').replace(/ż/g, 'z').replace(/ź/g, 'z')
+               .trim();
 }
 
 function sendMessageFromBot(messageText) {
@@ -56,9 +91,21 @@ function sendMessageFromBot(messageText) {
         </div>
         <div class="message-text">
             <p>${messageText}</p>
+            <div class="message-time">${getCurrentTime()}</div>
         </div>`;
-    chatMessages.appendChild(messageDiv);
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+    setTimeout(function() {
+        chatMessages.appendChild(messageDiv);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }, 1000);
 }
 
-let staticImagePath = document.getElementById('static-image-path').value;s
+let staticImagePath = document.getElementById('static-image-path').value;
+
+
+
+function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
